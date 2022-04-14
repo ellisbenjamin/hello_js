26f8aa860438363747e636192bcf8adf775f0754
ellisbenjamin <ellisbe@gmail.com>
2022-04-14 07:18:32 +0100

    ok

--- hello.tf
+++ hello.tf
@@ -1,3 +1,14 @@
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
 resource "cloudflare_worker_script" "this" {
   name = var.name 
   content = "hello.js"
